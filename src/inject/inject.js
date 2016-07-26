886d16cd6017b56940231ce2475bd5b42d52f59e
Dinakar Tumu <dinakar91@gmail.com>
2016-07-26 12:26:30 -0400

    Added dark mode for medium

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,8 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
+		var d=document.getElementsByClassName("homeContainer")
+		console.log(d);	
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
@@ -10,4 +11,7 @@ chrome.extension.sendMessage({}, function(response) {
 
 	}
 	}, 10);
+	
+   document.getElementsByClassName("homeContainer")[0].setAttribute("class", "democlass");
+
 });
